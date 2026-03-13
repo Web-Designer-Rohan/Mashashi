7dd86e3ddb51ce7bccdf00aca1266895497a0846
Web Designer Rohan <biswasrohan100@gmail.com>
2026-03-13 14:27:18 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-// Top-level build file
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    // Updated Hilt plugin ID and version for better compatibility
-    id("com.google.dagger.hilt.android") version "2.51" apply false 
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
@@ -12,4 +10,3 @@ plugins {
 extra["minSdkVersion"] = 26
 extra["targetSdkVersion"] = 34
 extra["compileSdkVersion"] = 34
-

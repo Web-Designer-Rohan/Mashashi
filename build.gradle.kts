a71ad1c9d87fcfbea6b9340ffff2e8c988972fa2
Web Designer Rohan <biswasrohan100@gmail.com>
2026-03-13 14:21:37 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
+// Top-level build file
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    id("com.google.dagger.hilt.android") version "2.5.1" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    // Updated Hilt plugin ID and version for better compatibility
+    id("com.google.dagger.hilt.android") version "2.51" apply false 
+    id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
 
-// Global variables for sub-projects
 extra["minSdkVersion"] = 26
 extra["targetSdkVersion"] = 34
 extra["compileSdkVersion"] = 34
+

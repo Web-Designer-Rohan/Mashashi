83d75f32fa78491b4d1012f9711c21be3e78c9f8
Web Designer Rohan <biswasrohan100@gmail.com>
2026-03-13 13:56:33 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,9 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-gradle.ext.minSdkVersion = 26
-gradle.ext.targetSdkVersion = 34
-gradle.ext.compileSdkVersion = 34
+// This file lives at the ROOT of your project
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.dagger.hilt.android") version "2.5.1" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+}
 
-gitProperties = [
-    'version': '1.0.0',
-    'commit': 'unknown'
-]
-
-// Add build scan plugin if needed
-// plugins {
-//     id 'com.gradle.enterprise' version '3.14.1'
-// }
-
-// Build scan configuration
-// gradleEnterprise {
-//     buildScan {
-//         termsOfServiceUrl = 'https://gradle.com/terms-of-service'
-//         termsOfServiceAgree = 'yes'
-//     }
-// }

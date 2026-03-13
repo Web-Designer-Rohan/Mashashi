d9b62e275073ffbc344d9673401eb71171a301b1
Web Designer Rohan <biswasrohan100@gmail.com>
2026-03-13 14:08:52 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
-// This file lives at the ROOT of your project
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("com.google.dagger.hilt.android") version "2.5.1" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
 
+// Global variables for sub-projects
+extra["minSdkVersion"] = 26
+extra["targetSdkVersion"] = 34
+extra["compileSdkVersion"] = 34
